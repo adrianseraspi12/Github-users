24f8c8bc2fc8919143464d75f6ca41b35ebacb5b
aj <adrianseraspi12@gmail.com>
2021-03-14 14:16:48 +0800

    Changed implementation of request from client

--- app/src/main/java/com/example/githubusers/data/remote/repository/GithubRepository.kt
+++ app/src/main/java/com/example/githubusers/data/remote/repository/GithubRepository.kt
@@ -1,67 +1,53 @@
 package com.example.githubusers.data.remote.repository
 
-import com.example.githubusers.data.remote.Listener
+import com.example.githubusers.data.local.Result
 import com.example.githubusers.data.remote.model.ProfileResponse
 import com.example.githubusers.data.remote.model.UserResponse
 import com.example.githubusers.data.remote.service.GithubClient
 import com.example.githubusers.util.constants.requestUserListErrorMessage
 import com.example.githubusers.util.constants.requestUserProfileErrorMessage
 import com.example.githubusers.util.constants.somethingWentWrongErrorMessage
-import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
-import retrofit2.Call
-import retrofit2.Callback
+import kotlinx.coroutines.*
 import retrofit2.Response
 
 class GithubRepository(
-    private var client: GithubClient,
-    private val ioDispatcher: CoroutineDispatcher = Dispatchers.IO
+        private var client: GithubClient,
+        private val ioDispatcher: CoroutineDispatcher = Dispatchers.IO
 ) : IGithubRepository {
 
-    override suspend fun requestUserList(
-        page: Int,
-        listener: Listener<List<UserResponse>>
-    ) = withContext(ioDispatcher) {
-        client.getUserList(page).enqueue(object : Callback<List<UserResponse>> {
-            override fun onResponse(
-                call: Call<List<UserResponse>>,
-                response: Response<List<UserResponse>>
-            ) {
-                //  Returns success when status code is between 200 and 299
-                if (response.code() in 200..299) {
-                    val listOfUserResponse = response.body()
-                    listener.onSuccess(listOfUserResponse)
-                }
-                listener.onFailed(requestUserListErrorMessage)
+    override suspend fun requestUserList(page: Int): Result<List<UserResponse>> = withContext(ioDispatcher) {
+        try {
+            val response = getRequestUserListResponse(page)
+            if (response.code() == 200) {
+                return@withContext Result.onSuccess(response.body())
             }
-
-            override fun onFailure(call: Call<List<UserResponse>>, t: Throwable) {
-                listener.onFailed(t.message ?: somethingWentWrongErrorMessage)
-            }
-        })
+            return@withContext Result.onFailed(requestUserListErrorMessage)
+        } catch (t: Throwable) {
+            return@withContext Result.onFailed(t.message ?: somethingWentWrongErrorMessage)
+        }
     }
 
-    override suspend fun requestUserProfile(
-        username: String,
-        listener: Listener<ProfileResponse>
-    ) = withContext(ioDispatcher) {
-        client.getProfile(username).enqueue(object : Callback<ProfileResponse> {
-            override fun onResponse(
-                call: Call<ProfileResponse>,
-                response: Response<ProfileResponse>
-            ) {
-                //  Check status code
-                if (response.code() == 200) {
-                    val userProfileResponse = response.body()
-                    listener.onSuccess(userProfileResponse)
-                }
-                listener.onFailed(requestUserProfileErrorMessage)
+    override suspend fun requestUserProfile(username: String): Result<ProfileResponse> = withContext(ioDispatcher) {
+        try {
+            val response = getRequestUserProfile(username)
+            val code = response.code()
+            if (code == 200) {
+                return@withContext Result.onSuccess(response.body())
             }
+            return@withContext Result.onFailed(requestUserProfileErrorMessage)
+        } catch (t: Throwable) {
+            return@withContext Result.onFailed(t.message ?: somethingWentWrongErrorMessage)
+        }
+    }
 
-            override fun onFailure(call: Call<ProfileResponse>, t: Throwable) {
-                listener.onFailed(t.message ?: somethingWentWrongErrorMessage)
-            }
-        })
+    //  Created a function without suspend to
+    //  Remove the warning "inappropriate blocking method call".
+    //  Jetbrains inspection issue "https://youtrack.jetbrains.com/issue/KTIJ-838"
+    private fun getRequestUserListResponse(page: Int): Response<List<UserResponse>> {
+        return client.getUserList(page).execute()
+    }
+
+    private fun getRequestUserProfile(username: String): Response<ProfileResponse> {
+        return client.getProfile(username).execute()
     }
 }
--- app/src/main/java/com/example/githubusers/data/remote/repository/IGithubRepository.kt
+++ app/src/main/java/com/example/githubusers/data/remote/repository/IGithubRepository.kt
@@ -1,12 +1,12 @@
 package com.example.githubusers.data.remote.repository
 
-import com.example.githubusers.data.remote.Listener
+import com.example.githubusers.data.local.Result
 import com.example.githubusers.data.remote.model.ProfileResponse
 import com.example.githubusers.data.remote.model.UserResponse
 
 interface IGithubRepository {
 
-    suspend fun requestUserList(page: Int, listener: Listener<List<UserResponse>>)
-    suspend fun requestUserProfile(username: String, listener: Listener<ProfileResponse>)
+    suspend fun requestUserList(page: Int): Result<List<UserResponse>>
+    suspend fun requestUserProfile(username: String): Result<ProfileResponse>
 
 }
