21f8e464b737a74d07360859db8873fe885d7384
aj <adrianseraspi12@gmail.com>
2021-03-14 18:13:57 +0800

    Fixed invalid url

--- app/src/main/java/com/example/githubusers/util/constants/ApiClient.kt
+++ app/src/main/java/com/example/githubusers/util/constants/ApiClient.kt
@@ -1,5 +1,8 @@
 package com.example.githubusers.util.constants
 
-const val users = "users/"
+const val users = "users"
 const val sinceQuery = "since"
 const val usernamePath = "username"
+const val authorizationHeader = "Authorization"
+//  For testing
+const val accessToken = "token 798ec6776f34908ecf09da316c5f5b7f31612f5b"
